68caaf90c9dd2e1c2cdd696a9564f3b7c630bab5
Grace Lindelien <gracelindelien@gmail.com>
2020-09-11 15:12:34 -0700

    Update Proxy to point to deployed services

--- server/index.js
+++ server/index.js
@@ -15,14 +15,14 @@ app.use('/:id', express.static('public'));
 app.use('/restaurant/:id', createProxyMiddleware({ target: 'http://54.193.61.185:3001', changeOrigin: true }));
 
 // // Info Sidebar
-// app.use('/:copyId/restaurants/:id', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true }));
+app.use('/:copyId/restaurants/:id', createProxyMiddleware({ target: 'http://3.101.107.222:3002', changeOrigin: true }));
 
 // // Tips & Recommendations
-// app.use('/api/tips/:id', createProxyMiddleware({ target: 'http://localhost:6070', changeOrigin: true }));
-// app.use('/api/articles/:id', createProxyMiddleware({ target: 'http://localhost:6070', changeOrigin: true }));
+app.use('/api/tips/:id', createProxyMiddleware({ target: 'http://3.22.130.18', changeOrigin: true }));
+app.use('/api/articles/:id', createProxyMiddleware({ target: 'http://3.22.130.18', changeOrigin: true }));
 
 // // Similar Restaurants
-// app.use('/restaurants/:id', createProxyMiddleware({ target: 'http://localhost:3004', changeOrigin: true }));
+app.use('/restaurants/:id', createProxyMiddleware({ target: 'http://3.17.70.45:3004', changeOrigin: true }));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
